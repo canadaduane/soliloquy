7eb86b38ed3c9c9de37768ffdc9b4e9af3e33800
Duane Johnson <duane.johnson@gmail.com>
2022-10-16 23:13:19 -0600

    Fix dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,21 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
+import alias from "@rollup/plugin-alias";
+
+import * as path from "path";
+
+const projectRootDir = path.resolve(__dirname);
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [
+    alias({
+      entries: [
+        { find: "~", replacement: path.resolve(projectRootDir, "src") },
+      ],
+    }),
+    svelte(),
+  ],
   build: {
     outDir: "build",
   },
