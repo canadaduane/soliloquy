d9ee0e7c17f3240112adeb1ab7be949b6149c14a
Duane Johnson <duane.johnson@gmail.com>
2022-10-22 13:12:54 -0600

    Update character discriptions; add isSelected

--- src/stores/characters.ts
+++ src/stores/characters.ts
@@ -3,106 +3,107 @@ import type { Character } from "../types";
 import { writable, type Writable } from "svelte/store";
 import Color from "colorjs.io";
 
-let color = -20;
+let color = 0;
 const lg = 0.9; // lightness
 const ch = 0.1; // chroma
-const step = 360 / 12;
+const step = 360 / 12; // divide the color space into 12
+
+function makeCharacter(traits: {
+  name: string;
+  color?: string;
+  image?: string;
+  details?: string;
+  isNew?: boolean;
+  isSelected?: boolean;
+}): Character {
+  return {
+    name: traits.name,
+    color: traits.color ?? new Color("oklch", [lg, ch, (color += step)]),
+    image: traits.image
+      ? traits.image.charAt(0) === "/"
+        ? traits.image
+        : `/archetypes/${traits.image}`
+      : null,
+    details: traits.details ?? null,
+    isNew: traits.isNew ?? false,
+    isSelected: traits.isSelected ?? false,
+  };
+}
 
 export const characters: Writable<Character[]> = writable([
-  {
+  makeCharacter({
     name: "Creator",
-    color: new Color("oklch", [lg, ch, (color += step)]),
-    image: "/archetypes/creator.webp",
+    image: "creator.webp",
     details:
       "The drive to express and create--to paint, compose, build, innovate.",
-    isNew: false,
-  },
+  }),
 
-  {
+  makeCharacter({
     name: "Explorer",
-    color: new Color("oklch", [lg, ch, (color += step)]),
-    image: "/archetypes/explorer.webp",
-    details: null,
-    isNew: false,
-  },
+    image: "explorer.webp",
+    details: "Wanderlust, novelty, curiosity.",
+  }),
 
-  {
+  makeCharacter({
     name: "Outlaw",
-    color: new Color("oklch", [lg, ch, (color += step)]),
-    image: "/archetypes/outlaw.webp",
-    details: null,
-    isNew: false,
-  },
+    image: "outlaw.webp",
+    details: "Rule-breaker, don't cross my boundaries.",
+  }),
 
-  {
+  makeCharacter({
     name: "Jester",
-    color: new Color("oklch", [lg, ch, (color += step)]),
-    image: "/archetypes/jester.webp",
-    details: null,
-    isNew: false,
-  },
+    image: "jester.webp",
+    details: "Comic, fun-lover, goof, enjoys the moment.",
+  }),
 
-  {
+  makeCharacter({
     name: "Lover",
-    color: new Color("oklch", [lg, ch, (color += step)]),
-    image: "/archetypes/lover.webp",
-    details: null,
-    isNew: false,
-  },
+    image: "lover.webp",
+    details:
+      "Giving yourself fully, entranced by life, possibly insane but ok with it.",
+  }),
 
-  {
+  makeCharacter({
     name: "Caregiver",
-    color: new Color("oklch", [lg, ch, (color += step)]),
-    image: "/archetypes/caregiver.webp",
-    details: null,
-    isNew: false,
-  },
+    image: "caregiver.webp",
+    details: "Expanding the self, duty and concern for others.",
+  }),
 
-  {
+  makeCharacter({
     name: "Everyman",
-    color: new Color("oklch", [lg, ch, (color += step)]),
-    image: "/archetypes/everyman.webp",
-    details: null,
-    isNew: false,
-  },
+    image: "everyman.webp",
+    details: "Get along, hang with friends, keep things simple & routine.",
+  }),
 
-  {
+  makeCharacter({
     name: "Innocent",
-    color: new Color("oklch", [lg, ch, (color += step)]),
-    image: "/archetypes/innocent.webp",
-    details: null,
-    isNew: false,
-  },
+    image: "innocent.webp",
+    details: "The wonder of first experiences, untainted and no judgment.",
+  }),
 
-  {
+  makeCharacter({
     name: "Ruler",
-    color: new Color("oklch", [lg, ch, (color += step)]),
-    image: "/archetypes/ruler.webp",
-    details: null,
-    isNew: false,
-  },
+    image: "ruler.webp",
+    details: "Order and prosperity, tight grip, avoid uncertainties.",
+  }),
 
-  {
+  makeCharacter({
     name: "Sage",
-    color: new Color("oklch", [lg, ch, (color += step)]),
-    image: "/archetypes/sage.webp",
-    details: null,
-    isNew: false,
-  },
+    image: "sage.webp",
+    details:
+      "Understanding of many generations, patience, influence without control.",
+  }),
 
-  {
+  makeCharacter({
     name: "Magician",
-    color: new Color("oklch", [lg, ch, (color += step)]),
-    image: "/archetypes/magician.webp",
-    details: null,
-    isNew: false,
-  },
+    image: "magician.webp",
+    details:
+      "Unexpected, unpredictable, you never know what's around the corner.",
+  }),
 
-  {
+  makeCharacter({
     name: "Hero",
-    color: new Color("oklch", [lg, ch, (color += step)]),
-    image: "/archetypes/hero.webp",
-    details: null,
-    isNew: false,
-  },
+    image: "hero.webp",
+    details: "Action is mine, I will make my mark, right wrongs.",
+  }),
 ]);
